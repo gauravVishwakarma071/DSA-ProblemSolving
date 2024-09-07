80ac7ee9dd7daac69d6459b25f66b0ef3bd9d8a9
Gaurav <gauravvishwakarma071@gmail.com>
2024-09-07 00:02:46 -0400

    an integer 'n'. Return 'true' if 'n' is an Armstrong number, and 'false' otherwise

---
+++ AmstrongNum.java
@@ -0,0 +1,23 @@
+// You are given an integer 'n'. Return 'true' if 'n' is an Armstrong number, and 'false' otherwise
+import java.util.Scanner;
+public class AmstrongNum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number...");
+        int n = sc.nextInt();
+        System.out.println(amNum(n));
+    }
+
+    private static boolean amNum(int n) {
+        int orignalNum  = n;
+        int numDigits = String.valueOf(n).length();  // Calculate the number of digits
+        int ans = 0;
+        while(n>0){
+            int dig = n % 10;
+            ans += Math.pow(dig, numDigits);  // Raise to the power of the number of digits
+            n = n / 10;
+        }
+        return orignalNum==ans;
+    }
+}
